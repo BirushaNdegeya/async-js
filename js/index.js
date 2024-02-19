b572d4a594f44840085886f99025336740f368fd
Birusha Ndegeya <142086785+BirushaNdegeya@users.noreply.github.com>
2024-02-19 20:50:57 +0200

    new commit

--- js/index.js
+++ js/index.js
@@ -1,14 +1,17 @@
 const API = 'https://jsonplaceholder.typicode.com/users';
-const APILINK = 'https://icanhazdadjoke.com/';
-
-// 2nd parameter of fetch
-
-const joke = async () => {
-   const resp = await fetch('https://icanhazdadjoke.com');
-   return await resp.json();
+const DAD_JOKE_API = 'https://icanhazdadjoke.com/';
+const TEST_API = 'https://httpbin.org/post/';
+const POST_API =  'https://api.icndb.com/jokes/random';
+const posts = { 
+   id: "ucapjGQZTf", 
+   joke: "What do you call a pig that knows karate? A pork chop!", 
+   status: 200 
 }
 
-const data = await joke();
-console.log(data);
+const dadJoke = async (first, last) => {
+   const resp = await fetch(`${POST_API}?firstName=${first}&lastName=${last}`);
+   const jsonResp = await resp.json();
+   console.log(jsonResp.value);
+};
 
-// 40
+dadJoke("Bruce","Lee");
